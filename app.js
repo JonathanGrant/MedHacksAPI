abe6c46eb8a5dfbc42971f9cd07b0f9e738a28f2
JonathanGrant <jonathanallengrant@gmail.com>
2016-09-23 21:11:06 -0700

    Never add node_moduels

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // error handlers
 
@@ -56,5 +56,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.get('/index', function(req, res) {
+    res.sendfile(path.join(__dirname + '/public/index.html'));
+});
 
 module.exports = app;
