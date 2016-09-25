13e7bf391073091249dbe9934386ef000dd2ea39
nhhoang96 <hn1174@messiah.edu>
2016-09-24 20:27:33 -0400

    adding submitreport page

--- app.js
+++ app.js
@@ -366,4 +366,8 @@ app.get('/sendfirsttext', function(req, res) {
     });
 })
 
+app.get('submitReport', function(req, res) {
+  res.sendfile(path.join(__dirname + '/public/submitReport.html'));
+})
+
 module.exports = app;
